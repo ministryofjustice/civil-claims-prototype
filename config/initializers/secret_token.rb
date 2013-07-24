e9355db1c52bc2b1df929529098ee64379d3572e
Tom Gladhill <whoojemaflip@gmail.com>
2013-07-24 12:22:56 +0100

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CivilClaims::Application.config.secret_key_base = 'fa4b3f04325570b53366c672215bcbf7ecbb647830b2dd909f7fcf12c68a3ccb712d4efadc7ab67c0ae642012c9934e39b083281619413162388931b70006480'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CivilClaims::Application.config.session_store :cookie_store, key: '_civil-claims_session'
