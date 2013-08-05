063799e8b72d16a34965c3e8fe93f056224f3f42
zenon3 <christopherduraj@gmail.com>
2013-08-05 12:04:17 +0100

    Added a little bit of interaction for attachments - adding new, removing from the page

--- app/assets/javascripts/attachment.js
+++ app/assets/javascripts/attachment.js
@@ -1,11 +1,18 @@
-$(document).ready(function() {
-	$('#attachment-select').click(function(){
-		$('#attachment-list').show('fast');
-	}
 
-});
+function remove_attachment(link) {
+  var parent_row = $(link).parent().parent(); 
+  // $(link).closest(".tr")
+  parent_row.find(":input").val("true").change();
+  parent_row.hide('fast');
 
+};
 
-  $(function() {
-    alert( "ready!" );
-  });
+function add_attachments() {
+  var att_index = $('tr:visible').length;
+  $('#attachments-table tr:last').after('\
+    <tr class="attachment-fields"> \
+      <input id="claim_attachments_attributes_'+att_index+'__destroy" name="claim[attachments_attributes]['+att_index+'][_destroy]" type="hidden" value="false"> \
+      <td><a href="#">Another document('+att_index+').doc</a></td> \
+      <td><a class="btn btn-danger btn-small" href="#" onclick="remove_attachment(this); return false;">Remove</a></td> \
+    </tr>');
+}
