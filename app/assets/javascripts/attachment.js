b6683c467ba82d73832c02402ffa996a76ab2499
zenon3 <christopherduraj@gmail.com>
2013-08-05 12:22:06 +0100

    Added file field name to file upload form

--- app/assets/javascripts/attachment.js
+++ app/assets/javascripts/attachment.js
@@ -9,10 +9,12 @@ function remove_attachment(link) {
 
 function add_attachments() {
   var att_index = $('tr:visible').length;
+  var file_name = 'Another document('+att_index+').doc'
   $('#attachments-table tr:last').after('\
     <tr class="attachment-fields"> \
       <input id="claim_attachments_attributes_'+att_index+'__destroy" name="claim[attachments_attributes]['+att_index+'][_destroy]" type="hidden" value="false"> \
-      <td><a href="#">Another document('+att_index+').doc</a></td> \
+      <input id="claim_attachments_attributes_'+att_index+'_file_name" name="claim[attachments_attributes]['+att_index+'][file_name]" type="hidden" value="'+file_name+'"> \
+      <td><a href="#">'+file_name+'</a></td> \
       <td><a class="btn btn-danger btn-small" href="#" onclick="remove_attachment(this); return false;">Remove</a></td> \
     </tr>');
 }
