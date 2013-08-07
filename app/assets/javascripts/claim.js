970b4cf71efe4ad37c71316442eaedf3dc6629a0
Tom Gladhill <whoojemaflip@gmail.com>
2013-08-07 12:57:53 +0100

    Added helper to generate inline help

--- app/assets/javascripts/claim.js
+++ app/assets/javascripts/claim.js
@@ -1,14 +1,18 @@
-$('#claims-index-tabs li.submit a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-});
+$(document).ready(function() {
 
-$('#claims-index-tabs li.list a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-});
+  $('#claims-index-tabs li.submit a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
 
-$('input.address-lookup').click(function (e) {
-  console.log('asd');
-  console.log(this.parent.html)
+  $('#claims-index-tabs li.list a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+  });
+
+  $('.inline-help .toggle-help').click(function(e) {
+      e.preventDefault();
+      var $collapse = $(this).closest('.collapse-group').find('.collapse').collapse('toggle');
+      $(this).toggleClass('toggle-hidden').toggleClass('toggle-visible');
+  });
 });
