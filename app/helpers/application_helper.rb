50eaf227736974319313eab9b21bb00db9674c99
zenon3 <christopherduraj@gmail.com>
2013-08-12 10:16:20 +0100

    Changed divs to sections following gov.uk design. Added fake modal dialog box for csv attachment and selecting an attachment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def button_to_remove_fields(name, f, association)
-    f.hidden_field(:_destroy) + button_to_function(name, "remove_fields(this,'#{association}')", class: "btn btn-danger btn-small")
+  def button_to_remove_fields(name, form, association)
+    form.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this,'#{association}')")
   end
   
-  def button_to_add_fields(directory, name, f, association)
-    new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render("#{directory}/#{association.to_s.singularize}_fields", :f => builder)
+  def button_to_add_fields(directory, name, form, association, default_values = nil)
+    new_object = form.object.class.reflect_on_association(association).klass.new
+    fields = form.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render "#{directory}/#{association.to_s.singularize}_fields", :f => builder, :default_values => default_values
     end
-    button_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", class: "btn btn-primary")
+    button_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")", class: "button")
   end
 
   def inline_date_input(form, date_field_symbol)
