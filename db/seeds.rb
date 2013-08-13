c205020e7e985bfda7f6a5f653445f9a934d44d0
Tom Gladhill <whoojemaflip@gmail.com>
2013-08-13 13:39:28 +0100

    Set default claimant to John Smith Reynolds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Claimant.create(Person.generate)
+Claimant.create({
+  title: 'Mr',
+  full_name: 'John Smith Reynolds',
+  phone: '+44 (0)207 613 4431',
+  mobile: '+44 (0)7792 685998',
+  email: 'jsreynolds@btconnect.com',
+  address: Address.create({
+    street_1: '121 Elm Park Road',
+    town: 'London',
+    postcode: 'SW1 1SZ'
+    })
+  })
 Defendant.create(Person.generate)
 Staff.create(Person.generate)
 Judge.create(Person.generate)
