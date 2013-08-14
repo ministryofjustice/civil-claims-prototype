cdee4f29c92279b0f9a22dfcde45ab1f90fdfa6f
Tom Gladhill <whoojemaflip@gmail.com>
2013-08-14 19:13:18 +0100

    Better random address generation

--- app/models/address.rb
+++ app/models/address.rb
@@ -15,11 +15,28 @@ class Address < ActiveRecord::Base
   end
 
   def self.generate()
+
     address = {
       :street_1   => Random.address_line_1,
-      :street_2   => Random.address_line_2,
       :postcode   => Random.uk_post_code,
       :town       => Random.city
     }
+
+    i = rand
+    if i > 0.6
+      address[:street_3] = Random.address_line_2
+    elsif i > 0.2
+      address[:street_2] = Random.address_line_2
+    else
+      address[:street_2] = Random.address_line_2
+      address[:street_3] = Random.address_line_2
+    end
+    address[:county] = random_county if rand > 0.3
+    address
+  end
+
+  def self.random_county
+    uk_county_arrray = ['Bedfordshire', 'Berkshire', 'Bristol', 'Buckinghamshire', 'Cambridgeshire', 'Cheshire', 'City of London', 'Cornwall', 'Cumbria', 'Derbyshire', 'Devon', 'Dorset', 'Durham', 'East Riding of Yorkshire', 'East Sussex', 'Essex', 'Gloucestershire', 'Greater London', 'Greater Manchester', 'Hampshire', 'Herefordshire', 'Hertfordshire', 'Isle of Wight', 'Kent', 'Lancashire', 'Leicestershire', 'Lincolnshire', 'Merseyside', 'Norfolk', 'North Yorkshire', 'Northamptonshire', 'Northumberland', 'Nottinghamshire', 'Oxfordshire', 'Rutland', 'Shropshire', 'Somerset', 'South Yorkshire', 'Staffordshire', 'Suffolk', 'Surrey', 'Tyne and Wear', 'Warwickshire', 'West Midlands', 'West Sussex', 'West Yorkshire', 'Wiltshire', 'Worcestershire', 'Anglesey', 'Brecknockshire', 'Caernarfonshire', 'Carmarthenshire', 'Cardiganshire', 'Denbighshire', 'Flintshire', 'Glamorgan', 'Merioneth', 'Monmouthshire', 'Montgomeryshire', 'Pembrokeshire', 'Radnorshire', 'Aberdeenshire', 'Angus', 'Argyllshire', 'Ayrshire', 'Banffshire', 'Berwickshire', 'Buteshire', 'Cromartyshire', 'Caithness', 'Clackmannanshire', 'Dumfriesshire', 'Dunbartonshire', 'East Lothian', 'Fife', 'Inverness-shire', 'Kincardineshire', 'Kinross', 'Kirkcudbrightshire', 'Lanarkshire', 'Midlothian', 'Morayshire', 'Nairnshire', 'Orkney', 'Peeblesshire', 'Perthshire', 'Renfrewshire', 'Ross-shire', 'Roxburghshire', 'Selkirkshire', 'Shetland', 'Stirlingshire', 'Sutherland', 'West Lothian', 'Wigtownshire', 'Antrim', 'Armagh', 'Down', 'Fermanagh', 'Londonderry', 'Tyrone' ]
+    uk_county_arrray.sample
   end
 end
