055a587aa7e6223c7b9c3d9b56505130b9a59544
Clive Murray <clive.murray@digital.justice.gov.uk>
2013-08-27 15:14:18 +0100

    precompile ALL the css

--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,11 @@ module CivilClaims
       claims-global.css
       claims-layout.css
       claims-components.css
+      claims-usernav.css
+      claims-tabs.css
+      claims-panels.css
+      claims-forms.css
+      claims-elements.css
 
       gov-static/gov-ie.js
     )
