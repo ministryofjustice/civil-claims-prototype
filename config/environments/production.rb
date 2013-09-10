5d8c8dfdc392c79f2a09d9c263fd46e066815a87
Tom Gladhill <whoojemaflip@gmail.com>
2013-09-10 15:59:53 +0100

    Precompile subfoldered components

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ CivilClaims::Application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'components')
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
