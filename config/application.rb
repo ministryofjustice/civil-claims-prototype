b9393d12986702257108ac091e44e052ef9f54bb
Clive Murray <hi+github@clivemurray.com>
2013-12-02 15:18:05 +0000

    Trying to fix the asset pipeline stuff for heroku

--- config/application.rb
+++ config/application.rb
@@ -49,14 +49,14 @@ module CivilClaims
       claims-global.css
       claims-layout.css
       claims-components.css
-      rebuild/claims-mixins
-      rebuild/claims-global
-      rebuild/claims-layout
-      rebuild/claims-components
-      rebuild/components/claims-forms
-      rebuild/components/claims-intro
-      rebuild/components/claims-usernav
-      rebuild/components/claims-progressnav
+      rebuild-claims-mixins
+      rebuild-claims-global
+      rebuild-claims-layout
+      rebuild-claims-components
+      rebuild-components/rebuild-claims-forms
+      rebuild-components/rebuild-claims-intro
+      rebuild-components/rebuild-claims-usernav
+      rebuild-components/rebuild-claims-progressnav
       
       gov-static/gov-ie.js
     )
