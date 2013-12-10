053cc9907f183d878c49aa2c11d85a06914500d6
Clive Murray <clive.murray@digital.justice.gov.uk>
2013-12-10 17:40:15 +0000

    Corrections, added JS insertion of commas into numbers

--- app/assets/javascripts/rebuild/demo.js
+++ app/assets/javascripts/rebuild/demo.js
@@ -21,6 +21,8 @@ moj.Modules.demo = (function() {
       removeFile,
       attachMiscFile,
       addRentTableRow,
+      formatNumber,
+      fixThousands,
 
       //elements
       postcodeButtons,
@@ -85,6 +87,10 @@ moj.Modules.demo = (function() {
     } );
     $( jsDisable ).filter( ':checked' ).each( function() {
       jsDisableClick( $( this ) );
+    } );
+
+    $( '.number' ).each( function() {
+      fixThousands( $( this ) );
     } );
   };
 
@@ -360,6 +366,24 @@ moj.Modules.demo = (function() {
     } else {
       $tablePanel.data( 'show', true );
     }
+  };
+
+  fixThousands = function( $el ) {
+    var v = $el.text();
+    $el.text( formatNumber( v ) );
+  };
+
+  formatNumber = function( num ) {
+    var x, x1, x2, rgx;
+    num += '';
+    x = num.split( '.' );
+    x1 = x[ 0 ];
+    x2 = x.length > 1 ? '.' + x[ 1 ] : '';
+    rgx = /(\d+)(\d{3})/;
+    while ( rgx.test( x1 ) ) {
+          x1 = x1.replace( rgx, '$1' + ',' + '$2' );
+      }
+      return x1 + x2;
   };
 
   // public
